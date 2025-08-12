c58d345ed9d98b908d78f92ce3bebbf1c5f031fb
Prajwal <prajwal1510j@gmail.com>
2025-08-12 20:05:59 +0530

    13: api filters sorting

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,7 +1,8 @@
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { Category } from '@/payload-types';
 import z from "zod";
-import type { Where } from "payload";
+import type { Sort, Where } from "payload";
+import { sortValues } from "../search-params";
 
 export const productsRouter = createTRPCRouter({
     getMany: baseProcedure
@@ -10,11 +11,27 @@ export const productsRouter = createTRPCRouter({
                 category: z.string().nullable().optional(),
                 minPrice: z.string().nullable().optional(),
                 maxPrice: z.string().nullable().optional(),
+                tags: z.array(z.string()).nullable().optional(),
+                sort: z.enum(sortValues).nullable().optional(),
             })
         )
         .query(async ({ ctx, input }) => {
 
             const where: Where = {}
+
+            let sort: Sort = '-createdAt'
+
+            if(input.sort === 'curated') {
+                sort = '-createdAt'
+            }
+
+            if(input.sort === 'hot_and_new') {
+                sort = '+createdAt'
+            }
+
+            if(input.sort === 'trending') {
+                sort = '-createdAt'
+            }
 
 
             if (input.minPrice && input.maxPrice) {
@@ -70,10 +87,17 @@ export const productsRouter = createTRPCRouter({
 
             }
 
+            if(input.tags && input.tags.length > 0) {
+                where["tags.name"] = {
+                    in: input.tags,
+                }
+            }
+
             const data = await ctx.db.find({
                 collection: 'products',
                 depth: 1, // Populate "category", "image"
                 where,
+                sort,
             })
 
             // return [{ hello: 'world' }]
